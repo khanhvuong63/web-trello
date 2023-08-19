fc87d35072cc0bb2cfabbff9bac98a1e24f03898
khanhvuong63 <106170850+khanhvuong63@users.noreply.github.com>
2023-08-19 17:29:13 +0700

    dark_light

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,77 @@
 import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import { Box } from '@mui/material'
 
-function App() {
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode) 
+  }
 
   return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{display:'flex', alignItems:'center', gap: '8px'}}>
+            <LightModeIcon fontSize='small'/>
+            Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+        <div style={{display:'flex', alignItems:'center', gap: '8px'}}>
+            <DarkModeOutlinedIcon fontSize='small'/>
+            Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{display:'flex', alignItems:'center', gap: 1}}>
+            <SettingsBrightnessIcon fontSize='small'/>
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+function App() {
+  return (
     <>
+      <ModeSelect />
+      <br />
+      <ModeToggle />
+      <br />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
