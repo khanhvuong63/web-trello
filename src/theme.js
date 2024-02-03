3dbb0d58b5aa76794f2b615a8cbdb99736bec60b
khanhvuong63 <106170850+khanhvuong63@users.noreply.github.com>
2024-02-03 17:22:42 +0700

    trello_board_content

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,15 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
 // Create a theme instance.
 const theme = extendTheme({
   trello:{
-    appBarHeight: '58px',
-    boardBarHeight:'60px'
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT
   },
   colorSchemes: {
     // light: {
@@ -31,7 +35,8 @@ const theme = extendTheme({
             height: '8px'
           },
           '*::-webkit-scrollbar-thumb': {
-            backgroundColor: '#dcdde1'
+            backgroundColor: '#dcdde1',
+            borderRadius:'8px'
           },
           '*::-webkit-scrollbar-thumb:hover': {
             backgroundColor: 'white'
@@ -53,6 +58,13 @@ const theme = extendTheme({
           color: theme.palette.primary.main,
           fontSize: '0.875rem'
         })
+      }
+    },
+    MuiTypography:{
+      styleOverrides:{
+        root:{
+          '&.MuiTypography-body1':{ fontSize: '0.875rem' }
+        }
       }
     },
     MuiOutlinedInput: {
